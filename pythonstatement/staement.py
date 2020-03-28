7d9a8f63f95729dc66b0e4557a1fd19bd7631f72
unknown <alamin258000@gmail.com>
2020-03-28 16:17:36 +0600

    important thing that i do not know before

---
+++ pythonstatement/staement.py
@@ -0,0 +1,50 @@
+# i do not observ it before true is print value but false is not
+# if True:
+#     print("Hi polin You are looking what")
+# if False:
+#      print("Hi polin You are looking what")
+#  i Do not know before that  True can use in this way
+# here i use those Opratore
+#  and
+# not
+# or
+# user='admin'
+# loged_in=False
+#
+# if user=="admin" or loged_in:
+#     print("Admin Page")
+# else:
+#     print("you are not admin")
+#     #
+#     # not example
+# if not  loged_in:
+#     print("please Log in first")
+# else:
+#     print("Welcome to Homepage")
+
+# is will use for object address location location
+# a=[1,2,3]
+# b=[1,2,3]
+# print(a==b)
+# b=a
+# print(id(a))
+# print(id(b))
+# # is will onlt treuu when i will assign b value with  i value
+# print(a is b)
+
+# python false condition check
+# 1.False
+# 2.none
+# 3.Zero any number is false
+# 4.empty sequence for example '',(),[]
+# 5.empty Mapping for example {}
+# condition =False
+# condition =None
+# condition =0
+# condition=''
+# condition={}
+condition='sdfdsf'
+if condition:
+    print('Evaluated the true')
+else:
+    print('evulation the false')
